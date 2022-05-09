d9eddb48f9844cdf819dd97b3899578cd5e3768c
Doug Jones <DSJR1284@GMAIL.COM>
2022-05-08 22:27:09 -0400

    set up word hint function

--- hangman.rb
+++ hangman.rb
@@ -15,21 +15,30 @@ class Hangman
         ]  
     end  
 
+    def word_hint
+        word_teaser = ""
+    
+        @word.first.size.times do
+            word_teaser += "_ "
+        end 
+
+        puts word_teaser
+
+    end 
+
     def begin
         #ask the user for a letter
     puts "Welcome to Hangman"
     puts "The Game has started"
+    word_hint
     puts "Your Word is #{ @word.first.size} letters long" 
-    word_teaser = ""
     
-    @word.first.size.times do
-        word_teaser += "_ "
     end 
     
-    puts "Your hint is: #{ @word.last}"
+    puts "Your hint is: #{ @word.last }"
     puts "Enter a letter"
     guess = gets.chomp 
-end    
+end
 
 end 
 
