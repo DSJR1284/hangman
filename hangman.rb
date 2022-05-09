a1ca6e110e11ee1c0b95aedce70dc0f9aa07d98c
Doug Jones <DSJR1284@GMAIL.COM>
2022-05-08 21:36:03 -0400

    set up word bank

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,23 @@
 class Hangman 
 
+    def initialize 
+        letters = ('a'..'z').to_a
+        words = words.sample 
+    end 
+
+    def words 
+        [
+            "tennis", "A game played on a court"
+            "baseball", "A game played in a diamond"
+            "football", "A game with two meanings"
+            "track&field", "A sport about speed"
+        ]   
+
+    def begin
+        
+    end 
+    
+
 
 
 
