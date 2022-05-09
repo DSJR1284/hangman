f8b2879791f3e8c8c03ae4b0e568e8e38b93e45c
Doug Jones <DSJR1284@GMAIL.COM>
2022-05-08 22:00:04 -0400

    test begin method

--- hangman.rb
+++ hangman.rb
@@ -2,23 +2,28 @@ class Hangman
 
     def initialize 
         letters = ('a'..'z').to_a
-        words = words.sample 
+        @word = words.sample 
     end 
 
     def words 
         [
-            "tennis", "A game played on a court"
-            "baseball", "A game played in a diamond"
-            "football", "A game with two meanings"
-            "track&field", "A sport about speed"
-        ]   
+            ["tennis", "A game played on a court"],
+            ["baseball", "A game played in a diamond"],
+            ["football", "A game with two meanings"],
+            ["track&field", "A sport about speed"],
+            ["basketball", "A gameplayed in and out of a gym"],
+        ]  
+    end  
 
     def begin
-        
+        #ask the user for a letter
+    puts "Welcome to Hangman"
+    puts "The Game has started.. your word hint is #{ @word.first }"
+    guess = gets.chomp
+    puts "You guessed #{guess}"        
     end 
-    
-
-
-
 
 end 
+
+game = Hangman.new
+game.begin
