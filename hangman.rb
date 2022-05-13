d1996f7dedf952759f84727620f4dc52b5d41120
Doug Jones <DSJR1284@GMAIL.COM>
2022-05-12 21:17:42 -0400

    walk thorugh code/test game

--- hangman.rb
+++ hangman.rb
@@ -1,81 +1,90 @@
-class Hangman 
+class Hangman
 
-    def initialize 
-        letters = ('a'..'z').to_a
-        @word = words.sample 
-        @tries = 7
-        @correct_guesses = []
-        @word_teaser = ""
+    def initialize
+    @word = words.sample
+    @lives = 7
+    @word_teaser = ""
 
-        @word.first.size.times do
-            @word_teaser += "_ "
-        end 
-    end 
+    @word.first.size.times do
+        @word_teaser += "_ "
+    end
+    end
 
-    def words 
-        [
-            ["tennis", "A game played on a court"],
-            ["baseball", "A game played in a diamond"],
-            ["football", "A game with two meanings"],
-            ["track&field", "A sport about speed"],
-            ["basketball", "A gameplayed in and out of a gym"],
-        ]  
-    end  
+    def words
+    [
+        ["tennis", "A game played on a court"],
+        ["baseball", "A game played in a diamond"],
+        ["football", "A game with two meanings"],
+        ["track&field", "A sport about speed"],
+        ["basketball", "A gameplayed in and out of a gym"],
+    ]
+    end
 
-    def print_hint last_guess = nil
-        update_hint(last_guess) unless last_guess.nil? 
-        puts @word_teaser
-    end 
+    def print_teaser(last_guess = nil)
+    update_teaser(last_guess) unless last_guess.nil?
+    puts @word_teaser
+    end
 
-    def update_hint
-        new_hint = @word_teaser.split
+    def update_teaser last_guess
+    new_teaser = @word_teaser.split
 
-        new_hint.each_with_index do |letter, index|
-            #replace blank values with corret leters in the word were trying to guess 
-            if letter == '_ ' && @word.first[index] == last_guess
-                new_hint[index] = last_guess
-            end 
+    new_teaser.each_with_index do |letter, index|
+        # replace blank values with guessed letter if matches letter in word
+        if letter == '_' && @word.first[index] == last_guess
+        new_teaser[index] = last_guess
         end
-        @word_teaser = new_hint.join(' ')
-    end 
+    end
+
+    @word_teaser = new_teaser.join(' ')
+    end
 
     def make_guess
-        if @tries > 0  
+    if @lives > 0
         puts "Enter a letter"
-        guess = gets.chomp 
-        # if the letter is not part of the word removed the letters from array 
-        good_guess = @word.first.include? guess 
+        guess = gets.chomp
 
-        if guess == 'quit'
-            puts "Thank You for playing. come back anytime."        
+        good_guess = @word.first.include? guess
+
+        if guess == "exit"
+        puts "Thank you for playing!"
+        
+        #if guess is longer than 1 letter
+        elsif guess.length > 1
+        puts "only guess 1 letter at a time please!"
+            make_guess
+        
         elsif good_guess
-            puts "Good Job...Try another letter"
-            @correct_guesses << guess
-            #removed correct guess from alphabet
-            @letters.delete guess 
-            print_hint guess
+        puts "You are correct!"
+
+        print_teaser guess
+
+        if @word.first == @word_teaser.split.join
+            puts "Congratulations... you have won this round!"
+        else
             make_guess
-        else 
-            @tries -= 1
-            puts "Sorry Wrong Letter you have #{ @tries } tries left. Try Again!"
-            make_guess
+    end
+        else
+        @lives -= 1
+        puts "Sorry... you have #{ @lives } lives left. Try again!"
+        make_guess
         end
-            else
-                puts "Game Over"
-        end 
-    end 
+    else
+        puts "Game over... better luck next time!"
+    end
+    end
 
     def begin
-        #ask the user for a letter
-        puts "Welcome to Hangman"
-        puts "The Game has started"
-        puts "To exit game at any point type 'quit'"
-        print_hint    
-        puts "Your Word is #{ @word.first.size} letters long"    
-        puts "Your hint is: #{ @word.last }" 
-        make_guess    
+    # ask user for a letter
+    puts "New game started... your word is #{ @word.first.size } characters long"
+    puts "To exit game at any point type 'exit'"
+    print_teaser
+
+    puts "Clue: #{ @word.last }"
+
+    make_guess
     end
-end 
+
+end
 
 game = Hangman.new
 game.begin
